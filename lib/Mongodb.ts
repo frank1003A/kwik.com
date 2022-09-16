78fb584b0c0e4eede983c1fea579d886555ba3a6
Frank Ezene <ezenefrankc@gmail.com>
2022-09-16 11:12:20 +0100

    introduced mongo uri

--- lib/Mongodb.ts
+++ lib/Mongodb.ts
@@ -1,10 +1,10 @@
 import { MongoClient } from "mongodb";
 
-if (!process.env.CLOUD_DB) {
+if (!process.env.MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
-const uri: string = process.env.CLOUD_DB;
+const uri: string = process.env.MONGODB_URI;
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
